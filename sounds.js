3398a62445e182da7733c737374592f5df1b6b1a
ainimeid-v <vianart71ae@gmail.com>
2026-02-18 01:08:56 +0700

    Create sounds.js

---
+++ sounds.js
@@ -0,0 +1,9 @@
+function playMatchSound() {
+    const audio = new Audio("https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg");
+    audio.play();
+}
+
+function playWinSound() {
+    const audio = new Audio("https://actions.google.com/sounds/v1/cartoon/concussive_drum_hit.ogg");
+    audio.play();
+}
